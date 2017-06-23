70f7e8a89ad71d5ed886aedd302185b37fe9a6e3
Hernan Mendez <hernan21112001@gmail.com>
2017-06-23 18:05:29 -0400

    making a test

--- precomp.js
+++ precomp.js
@@ -1,6 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-
+//love
 localStorage.clear();
 
 localStorage.setItem('_codepen.io_hernanmendez_roguelike_customGames',JSON.stringify([{
